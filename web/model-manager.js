4051444c11bac5ecf4c49c9322c9fe6660504008
Christian Bastian <80225746+cdb-boop@users.noreply.github.com>
2024-03-31 04:52:10 -0400

    Revert enter to select and submit.

--- web/model-manager.js
+++ web/model-manager.js
@@ -991,8 +991,6 @@ class DirectoryDropdown {
                 else if (e.key === "Enter") {
                     e.stopPropagation();
                     const input = e.target
-                    /*
-                    // enter is for search
                     const selection = options[iSelection];
                     if (selection !== undefined && selection !== null) {
                         DirectoryDropdown.selectionToInput(
@@ -1007,7 +1005,6 @@ class DirectoryDropdown {
                         }
                         updateCallback();
                     }
-                    */
                     await submitCallback();
                     input.blur();
                 }
