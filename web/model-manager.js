5d7ffb0cf3a20859c75d120e29c935623f6af119
Christian Bastian <80225746+cdb-boop@users.noreply.github.com>
2024-07-24 02:36:35 -0400

    Fixed bug with object key-value.

--- web/model-manager.js
+++ web/model-manager.js
@@ -23,11 +23,10 @@ function request(url, options = undefined) {
 async function loadWorkflow(url) {
     const uri = (new URL(url)).searchParams.get("uri");
     const fileNameIndex = Math.max(uri.lastIndexOf("/"), uri.lastIndexOf("\\")) + 1;
-    const fileName = uri.substring(fileNameIndex);
+    const fileName = uri.substring(0, fileNameIndex);
     const response = await fetch(url);
     const data = await response.blob();
-    const datatype = data.type;
-    const file = new File([data],  fileName, { datatype });
+    const file = new File([data],  fileName, { type: data.type });
     app.handleFile(file);
 }
 
